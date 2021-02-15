4309852742923dd3c26f905baac08381e62637d2
Tom Weir <tpdweir@gmail.com>
2021-02-15 17:14:19 +0000

    added new host

--- tpdw/settings.py
+++ tpdw/settings.py
@@ -29,7 +29,7 @@ except:
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['127.0.0.1','.herokuapp.com','tpdw.uk']
+ALLOWED_HOSTS = ['127.0.0.1','.herokuapp.com','tpdw.uk', 'www.tpdw.uk']
 
 
 # Application definition
